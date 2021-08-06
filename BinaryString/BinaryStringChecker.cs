173cb64e6817854ba6c26c5b8979bd48c8ce6597
Ionel Stefanuca <ionel2988@gmail.com>
2021-08-06 10:36:41 +0800

    initial commit

---
+++ BinaryString/BinaryStringChecker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryString
+{
+    class BinaryStringChecker
+    {
+        public bool IsGood(string binaryString)
+        {
+            int oneCount = 0;
+            int zeroCount = 0;
+            for (var i = 0; i < binaryString.Length; i++)
+            {
+                if (binaryString[i] == '1')
+                {
+                    oneCount++;
+                }
+                else
+                {
+                    zeroCount++;
+                }
+
+                if (oneCount < zeroCount)
+                {
+                    return false;
+                }
+            }
+
+            return oneCount == zeroCount;
+        }
+    }
+}
---
+++ BinaryString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BinaryString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var checker = new BinaryStringChecker();
+
+            Console.WriteLine(checker.IsGood("11010"));
+            Console.WriteLine(checker.IsGood("11100010"));
+        }
+    }
+}
